718aa66921ce89f3ccff9bbe644780cd821078be
Alberic2001 <alberic.ziangbe@ism.edu.sn>
2020-05-09 13:26:52 +0000

    Mon projet Quizz

---
+++ myProjectQuizz/integration/connexion.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Page de connexion - Quizz</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="colors.css">
+</head>
+  <body class="d-flex flex-direction-column align-items-center justify-content-center opensans p-0 container-fluid" style="background-image: url(Images/img-bg.jpg); background-repeat: no-repeat; background-attachment:fixed; background-size:cover; background-position-x:center; background-position-y:center;">
+    <div class="w-100 h-auto container-fluid d-flex align-items-center justify-content-center bgcolor1">
+        <div class=" p-0 row-8 col-5 bg-white m-auto">
+            <h2 class="row-3 m-0 col-12 text-white w-100 bgcolor1">
+                Login Form
+            </h2>
+            <div class="row-9">
+                <div class="row-4 form-group">
+                  <input type="text" class="form-control" name="" placeholder="">
+                </div>
+                <div class="row-4 form-group">
+                  <input type="text" class="form-control" name="" id="" placeholder="">
+                </div>
+                <div>
+                  <button type="submit" name="btn_submit" id="" class="row-3 btn bgcolor1">Connexion</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
